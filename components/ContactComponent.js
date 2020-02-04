bfb585801dfe730ed29945b49e3ca0c7858882f5
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-02-04 09:43:03 -0600

    add content to Contact component

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
-import createStackNavigator from 'react-navigation/src/navigators/createStackNavigator';
-
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
 
 class Contact extends Component {
@@ -13,7 +12,17 @@ class Contact extends Component {
   render() {
     return (
       <ScrollView>
+        <Card 
+          title="Contact Information"
+          wrapperStyle={{margin: 20}}>
+            <Text>1 Nucamp Way</Text>
+            <Text>Seattle, WA 98001</Text>
+            <Text
+              style={{marginBottom: 10}}>U.S.A</Text>
+            <Text>Phone: 1-206-555-1234</Text>
+            <Text>Email: campsites@nucamp.co</Text>
 
+        </Card>
       </ScrollView>
     );
   }
