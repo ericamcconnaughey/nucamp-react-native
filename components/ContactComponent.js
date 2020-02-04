990c99d97c96e8b46f398f11a7e30a8ad644c2ec
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-02-04 09:34:45 -0600

    add About and Contact components with navigation

---
+++ components/ContactComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import createStackNavigator from 'react-navigation/src/navigators/createStackNavigator';
+
+
+
+class Contact extends Component {
+
+  static navigationOptions = {
+    title: 'Contact Us'
+  }
+
+  render() {
+    return (
+      <ScrollView>
+
+      </ScrollView>
+    );
+  }
+
+}
+
+export default Contact;
