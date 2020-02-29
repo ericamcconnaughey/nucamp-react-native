39a416fffbd4bc78c7be1e671499200eed954143
Erica McConnaughey <ericamcconnaughey@gmail.com>
2020-02-29 11:17:07 -0600

    add ImageManipulator to resize and convert login image as a PNG

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 
 class LoginTab extends Component {
@@ -150,7 +151,31 @@ class RegisterTab extends Component {
       });
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
-        this.setState({imageUrl: capturedImage.uri})
+        this.processImage(capturedImage.uri);
+      }
+    }
+  }
+
+  processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri, [{resize:{width: 400, height: 400}}], {format: ImageManipulator.SaveFormat.PNG});
+
+    console.log(processedImage);
+
+    this.setState({imageUrl: processedImage.uri})
+  }
+
+  getImageFromGallery = async () => {
+    const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    
+    if(cameraRollPermission.status === 'granted') {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowEditing: true,
+        aspect: [1, 1]
+      });
+      if (!capturedImage.cancelled) {
+        console.log(capturedImage);
+        this.processImage(capturedImage.uri);
       }
     }
   }
@@ -181,6 +206,9 @@ class RegisterTab extends Component {
             <Button 
               title='Camera'
               onPress={this.getImageFromCamera} />
+            <Button 
+              title='Gallery'
+              onPress={this.getImageFromGallery} />
           </View>
           <Input
             placeholder='Username'
--- redux/comments.js
+++ redux/comments.js
@@ -1,6 +1,8 @@
 import * as ActionTypes from './ActionTypes';
 
-export const comments = (state = { errMess: null, comments: []}, action) => {
+export const comments = (state = { errMess: null, 
+                                    comments: []}, 
+                                    action) => {
     switch (action.type) {
         case ActionTypes.ADD_COMMENTS:
             return {...state, errMess: null, comments: action.payload};
